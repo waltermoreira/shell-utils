e400f880fa6583dc29c08e14d9c3dbefcbb3668d
Walter Moreira <walter@waltermoreira.net>
2025-09-05 12:32:31 -0500

    Add git

--- flake.nix
+++ flake.nix
@@ -83,6 +83,7 @@
                   zsh-autosuggestions
                   zshBin
                   lesspipe
+                  git
                 ]);
               new_params = builtins.removeAttrs params [
                 "starshipConfig"
